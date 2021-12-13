9ac544ec6460d0796330bc7ffd35aa6a0e126b9d
BarryDing <barrydoooit@gmail.com>
2021-12-14 00:16:36 +0800

    first commit

---
+++ src/preprocessing.py
@@ -0,0 +1,49 @@
+from enum import Enum
+import pandas as pd
+import numpy as np
+
+
+class Symbol(Enum):
+    UP = 0,
+    DOWN = 1,
+    LEVEL = 2
+
+
+def read_data(file_name='../data/stacked_data.csv', drop_columns=("open", "high", "low", "volume",)):
+    dfall = pd.read_csv(file_name).drop(columns=list(drop_columns))
+    return dfall
+
+
+def parse_data(dfall):
+    sids = dfall.stock_id.unique()
+    df_parallel = pd.DataFrame()
+    df_parallel['tdate'] = dfall[dfall.stock_id == 13]['tdate']
+
+    def add_symbol(stock_id, threshold=0.01):
+        df_one = dfall[dfall.stock_id == sid].drop(columns=['stock_id'])
+        close_one = df_one.close.to_numpy()
+        close_diff_one = np.diff(close_one)
+        symbols = []
+        for diff, pre_val in zip(close_diff_one, close_one[:-1]):
+            vrc = diff/pre_val
+            if abs(vrc) < threshold:
+                symbols.append(Symbol.LEVEL)
+            elif vrc > 0:
+                symbols.append(Symbol.UP)
+            else:
+                symbols.append(Symbol.DOWN)
+        df_one = df_one.iloc[1:,:]
+        df_one = df_one.rename(columns={'close': 'close_%d' % stock_id})
+        df_one['symbol_%d' % stock_id] = symbols
+        return df_one
+
+    for sid in sids:
+        df_parallel = pd.merge(df_parallel, add_symbol(sid), how='left',
+                               on=['tdate'])
+
+    print(df_parallel.tail())
+
+
+if __name__ == '__main__':
+    dfall = read_data()
+    parse_data(dfall)
